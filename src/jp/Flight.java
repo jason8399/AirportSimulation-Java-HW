0a3d069ec0bf9fde006f33748167ddf473e53744
JasonPan <jason8399@gmail.com>
2015-03-29 16:28:21 +0800

    Override compareTo()

--- src/jp/Flight.java
+++ src/jp/Flight.java
@@ -3,5 +3,59 @@ package jp;
 /**
  * Created by JasonPan on 3/29/15.
  */
-public class Flight {
+public class Flight implements Comparable<Flight>{
+    private static int count;
+    private int flghitNo;
+    private int fuel;
+    private int waitTime;
+    private boolean isArrival;
+
+    {
+        count = 0;
+    }
+
+    Flight(){
+        count++;
+        this.flghitNo = count;
+        this.fuel = 25;
+        this.waitTime = 0;
+        this.isArrival = true;
+    }
+
+    @Override
+    public int compareTo(Flight flight) {
+        return 0;
+    }
+
+    public int getFlghitNo() {
+        return flghitNo;
+    }
+
+    public void setFlghitNo(int flghitNo) {
+        this.flghitNo = flghitNo;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getWaitTime() {
+        return waitTime;
+    }
+
+    public void setWaitTime(int waitTime) {
+        this.waitTime = waitTime;
+    }
+
+    public boolean isArrival() {
+        return isArrival;
+    }
+
+    public void setIsArrival(boolean isArrival) {
+        this.isArrival = isArrival;
+    }
 }
